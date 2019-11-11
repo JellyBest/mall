f69286645ef8c7cccd75b1ffcd60daef08d55973
jelly_liu <1113713492@qq.com>
2019-11-11 21:38:39 +0800

    car

--- pages/car/car.js
+++ pages/car/car.js
@@ -1,60 +1,19 @@
-// pages/cate/cate.js
+// pages/car/car.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    step:2,
     imageURL: "../../../assets/carrot.png",
-    active: 0,
-    cateList: [
-      {
-        categoryCode: "1",
-        categoryName: "时令蔬菜"
-      },
-      {
-        categoryCode: "2",
-        categoryName: "时令蔬菜"
-      },
-      {
-        categoryCode: "3",
-        categoryName: "时令蔬菜"
-      },
-      {
-        categoryCode: "4",
-        categoryName: "时令蔬菜"
-      },
-      {
-        categoryCode: "5",
-        categoryName: "时令蔬菜"
-      }
-    ]
-  },
-  changeTab(e){
-    let code = e.currentTarget.dataset.code
-    this.setData({
-      active: code
-    })
-    console.log(e)
-  },
-  onChange(event) {
-    wx.showToast({
-      icon: 'none',
-      title: `切换至第${event.detail}项`
-    });
-  },
-  initData(){
-    let code = this.data.cateList[0].categoryCode
-    this.setData({
-      active: code
-    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.initData()
+
   },
 
   /**
